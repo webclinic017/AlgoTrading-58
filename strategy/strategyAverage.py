6a131400b4c4b14a89de717e739c2a4dae0988e5
JY20 <jimmyyan1337@gmail.com>
2023-04-07 21:58:22 -0400

    Adding new average algo and sp500 testing

---
+++ strategy/strategyAverage.py
@@ -0,0 +1,54 @@
+import backtrader as bt
+
+
+class strategyBob2(bt.Strategy):
+
+    def log(self, txt, dt=None):
+        ''' Logging function fot this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+        self.order = None
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            return
+        if order.status in [order.Completed]:
+            # if order.isbuy():
+            #     self.log('BUY EXECUTED {}'.format(order.executed.price))
+            # elif order.issell():
+            #     self.log('SELL EXECUTED {}'.format(order.executed.price))
+            self.bar_executed = len(self)
+        self.order = None
+
+    def highest(self, num):
+        highestPrice = self.dataclose[0]
+        for i in range(num):
+            if(self.dataclose[-i] > highestPrice):
+                highestPrice = self.dataclose[-i]
+        return highestPrice
+
+    def lowest(self, num):
+        lowestPrice = self.dataclose[0]
+        for i in range(num):
+            if(self.dataclose[-i] < lowestPrice):
+                lowestPrice = self.dataclose[-i]
+        return lowestPrice
+
+    def next(self):
+        # self.log('Close, %.2f' % self.dataclose[0])
+
+        if self.order:
+            return
+
+        days = 5
+        if not self.position:
+            if(self.dataclose[0] <= self.lowest(days)):
+                temp = int(self.broker.get_cash()/self.dataclose[0])
+                self.order = self.buy(size=temp-100)
+        else:
+            if(self.dataclose[0] > self.position.price*1.1 and self.dataclose[0] >= self.highest(days)):
+                self.order = self.sell(size=self.position.size)
--- traderMain.py
+++ traderMain.py
@@ -1,13 +1,15 @@
+import time
 import yfinance as yf
 import backtrader as bt
 import pandas as pd
 import datetime
-import strategyBob
+import strategy.strategyBob as strategyBob
+import strategy.strategyAverage as strategyAverage
 import matplotlib
-import AnalyzerSuite as AnalyzerSuite
+import strategy.AnalyzerSuite as AnalyzerSuite
 
 
-def runStrategy(symbol, cash=100000, comission=0.01):
+def runStrategy(symbol, printEnable=False, plotMode=False, cash=100000, comission=0.01):
     cerebro = bt.Cerebro()
     cerebro.broker.setcash(cash)
     cerebro.broker.setcommission(comission)
@@ -16,25 +18,53 @@ def runStrategy(symbol, cash=100000, comission=0.01):
         symbol, '2010-01-01', '2020-01-01', auto_adjust=True))
 
     cerebro.adddata(data)
-    cerebro.addstrategy(strategyBob.strategyBob1)
+    # cerebro.addstrategy(strategyBob.strategyBob1)
+    cerebro.addstrategy(strategyAverage.strategyBob2)
 
-    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    if(printEnable):
+        print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
     cerebro.run()
 
     AnalyzerSuite.AnalyzerSuite.defineAnalyzers(AnalyzerSuite, cerebro)
     thestrats = cerebro.run(stdstats=True)
-    print(AnalyzerSuite.AnalyzerSuite.returnAnalyzers(
-        AnalyzerSuite, thestrats))
-    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
-    # cerebro.plot()
+    if(printEnable):
+        print(AnalyzerSuite.AnalyzerSuite.returnAnalyzers(
+            AnalyzerSuite, thestrats))
+        print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    if(plotMode):
+        cerebro.plot()
+
+    outputs = AnalyzerSuite.AnalyzerSuite.returnAnalyzers(
+        AnalyzerSuite, thestrats)
+    outputs['Final Value'] = cerebro.broker.getvalue()
+    #outputs['Symbol'] = symbol
+    return(outputs)
 
 
 if __name__ == '__main__':
-    list = ['BN.TO', 'TD.TO', 'ENB.TO']
-    list = ['BN.TO', 'BNS.TO']
-    # tickers = pd.read_html(
-    #     'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
-    # print(tickers.head())
-    # print(tickers.Symbol.to_list())
+    start = time.time()
+    list = ['BN.TO', 'TD.TO', 'ENB.TO', 'BNS.TO']
+    list = ['DELL']
+    readFile = open("./testing/sample/sampleList.txt", "r")
+    #list = readFile.read().split('\n')
+    readFile.close()
+
+    writeFile = open("./testing/sample/resultSample.txt", "a")
+
+    count = 1
     for stock in list:
-        runStrategy(stock)
+        print(str(count)+"/"+str(len(list)))
+        try:
+            result = runStrategy(stock, True, True)
+            if(result['DrawDown'] < 30 and result['Sharpe Ratio:'] > 1):
+                writeFile.write("Positive: "+str(stock)+" : "+str(result)+"\n")
+            else:
+                writeFile.write("Netural: "+str(stock)+" : "+str(result)+"\n")
+        except:
+            writeFile.write("Negative: "+str(stock)+" : " +
+                            "Blame Jimmy for not fixing these errors"+"\n")
+            print("Jimmy is too lazy to fix this")
+        count += 1
+    writeFile.close()
+    end = time.time()
+    print(end - start)
